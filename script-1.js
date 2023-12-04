89c07972cbd2d63249463af41d632bd6dd2f9ca3
“xyana” <xyanaxy@protonmail.com>
2023-12-04 20:50:23 +0000

    untracked

---
+++ script-1.js
@@ -0,0 +1,9 @@
+
+    document.getElementById('myonoffswitch').addEventListener("change", function(){
+        if (this.checked) window.location.href = 'https://xyana-xyz.netlify.app/index_grid.html';
+    });
+
+
+    document.getElementById('myonoffswitch2').addEventListener("change", function(){
+        if (this.checked) window.location.href = 'https://xyana-xyz.netlify.app/index.html';
+    });
---
+++ script.js
@@ -0,0 +1,44 @@
+const canvas = document.querySelector('#drawingCanvas');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const pinkCircle = document.createElement('img');
+const greenCircle = document.createElement('img');
+const blackCircle = document.createElement('img');
+
+pinkCircle.src = 'images/pink.png';
+greenCircle.src = 'images/green.png';
+blackCircle.src = 'images/black.png';
+
+let paintbrush = greenCircle;
+
+const context = canvas.getContext('2d');
+
+const handleDraw = (event) => {
+  
+  const drawEvent = event.type.startsWith('touch') ? event.touches[0] : event;
+  const left = drawEvent.clientX;
+  const top = drawEvent.clientY;
+
+  context.drawImage(paintbrush, left, top);
+}
+
+const handleClick = () => {
+  if (paintbrush === greenCircle) {
+    paintbrush = blackCircle;
+  } else if (paintbrush === blackCircle) {
+    paintbrush = pinkCircle;
+  } else if (paintbrush === pinkCircle) {
+    paintbrush = greenCircle;
+  }
+}
+
+canvas.addEventListener('click', handleClick);
+
+// Add both mouse and touch event listeners
+canvas.addEventListener('mousemove', handleDraw);
+canvas.addEventListener('mousedown', handleClick); 
+
+canvas.addEventListener('touchmove', handleDraw);
+canvas.addEventListener('touchstart', handleClick);
