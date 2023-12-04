91a43c95c9bfdebc071e6c209fcd872aaa2b4548
“xyana” <xyanaxy@protonmail.com>
2023-12-04 20:46:38 +0000

    untracked?

--- script-1.js
+++
@@ -1,9 +0,0 @@
-
-    document.getElementById('myonoffswitch').addEventListener("change", function(){
-        if (this.checked) window.location.href = 'https://xyana-xyz.netlify.app/index_grid.html';
-    });
-
-
-    document.getElementById('myonoffswitch2').addEventListener("change", function(){
-        if (this.checked) window.location.href = 'https://xyana-xyz.netlify.app/index.html';
-    });
