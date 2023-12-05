26b0a86099422870a5fd675244bfad8463adf5e7
“xyana” <xyanaxy@protonmail.com>
2023-12-05 11:33:07 +0000

    drawing-canvas

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const canvas = document.querySelector('#drawingCanvas');
-
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
@@ -12,16 +11,30 @@ greenCircle.src = 'images/green.png';
 blackCircle.src = 'images/black.png';
 
 let paintbrush = greenCircle;
+let drawingData = []; // Array to store drawing data
+
+// Load drawing data from local storage on page load
+window.onload = () => {
+  const savedDrawingData = localStorage.getItem('drawingData');
+  if (savedDrawingData) {
+    drawingData = JSON.parse(savedDrawingData);
+    replayDrawings();
+  }
+};
 
 const context = canvas.getContext('2d');
 
 const handleDraw = (event) => {
-  
   const drawEvent = event.type.startsWith('touch') ? event.touches[0] : event;
   const left = drawEvent.clientX;
   const top = drawEvent.clientY;
 
   context.drawImage(paintbrush, left, top);
+
+  drawingData.push({ left, top, paintbrush: paintbrush.src });
+
+  // Save drawing data to local storage
+  localStorage.setItem('drawingData', JSON.stringify(drawingData));
 }
 
 const handleClick = () => {
@@ -34,11 +47,22 @@ const handleClick = () => {
   }
 }
 
+const replayDrawings = () => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
+  drawingData.forEach((data) => {
+    const { left, top, paintbrush: paintbrushSrc } = data;
+    const img = new Image();
+    img.src = paintbrushSrc;
+    context.drawImage(img, left, top);
+  });
+}
+
+// Event listeners
 canvas.addEventListener('click', handleClick);
 
-// Add both mouse and touch event listeners
 canvas.addEventListener('mousemove', handleDraw);
-canvas.addEventListener('mousedown', handleClick); 
+canvas.addEventListener('mousedown', handleClick);
 
 canvas.addEventListener('touchmove', handleDraw);
 canvas.addEventListener('touchstart', handleClick);
